05654e689886d144a0bc96e2aaa38cb9188859cf
Andrew Haines <ajh21@sussex.ac.uk>
2012-10-17 10:47:38 +0100

    Adding location service wrapper

--- prototype/trunk/src/main/java/uk/ac/sussex/asegr3/prototype/HelloAndroidActivity.java
+++ prototype/trunk/src/main/java/uk/ac/sussex/asegr3/prototype/HelloAndroidActivity.java
@@ -1,8 +1,11 @@
 package uk.ac.sussex.asegr3.prototype;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class HelloAndroidActivity extends MapActivity {
 	
@@ -16,6 +19,19 @@ public class HelloAndroidActivity extends MapActivity {
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+        
+        LocationService locationService = new LocationServiceFactory().create(this);
+
+        // Check if enabled and if not send user to the GSP settings
+        // Better solution would be to display a dialog and suggesting to 
+        // go to the settings
+        if (!locationService.hasRequiredPermissions()) {
+          Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+          startActivity(intent);
+        } 
     }
 	@Override
 	protected boolean isRouteDisplayed() {

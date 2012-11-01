e5c2a2a9342f9d7e961d82b6483176ad975632ac
Preshit Ninawe <preshitninawe@rediffmail.com>
2012-11-01 22:11:57 +0000

    Adding sql to build

--- tracker/trunk/server/src/main/java/uk/ac/sussex/asegr3/tracker/server/dao/TrackerDao.java
+++ tracker/trunk/server/src/main/java/uk/ac/sussex/asegr3/tracker/server/dao/TrackerDao.java
@@ -1,6 +1,8 @@
 package uk.ac.sussex.asegr3.tracker.server.dao;
 
 import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.SqlBatch;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 
@@ -9,6 +11,9 @@ public interface TrackerDao {
 	  @SqlUpdate("insert into location (user_id, latitude, longitude, timestamp) values (:user_id, :latitude, :longitude, :timestamp)")
 	  void insert(@Bind("user_id") int userId, @Bind("latitude") double lat, @Bind("longitude") double lng, @Bind("timestamp") long timestamp ); 
 
+	  //@SqlBatch("insert into location (user_id, latitude, longitude, timestamp) values (:user_id, :latitude, :longitude, :timestamp)")
+	  //void insert(@BindBean Iterable<LocationDTO> inserts);
+	  
 	  @SqlQuery("select username from user where id = :id")
 	  String findNameById(@Bind("id") int id) ;
 }
